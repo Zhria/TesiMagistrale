01d9a4c16daa35cf73a545796b54e259a4af8231
Zhria <traka.zakaria@gmail.com>
2025-08-31 18:57:14 +0200

    push automatico

--- e2sim/src/SCTP/e2sim_sctp.cpp
+++ e2sim/src/SCTP/e2sim_sctp.cpp
@@ -176,7 +176,7 @@ int sctp_start_client(const char *server_ip_str, const int server_port)
 
     // Attendi il completamento della connect
     struct pollfd p{ .fd = fd, .events = POLLOUT, .revents = 0 };
-    rc = poll(&p, 1, 5000); // timeout 5s
+    rc = poll(&p, 1, -1); // bloccante  
     if (rc == 0) {
         fprintf(stderr, "FAILED (timeout 5000 ms)\n");
         close(fd);
