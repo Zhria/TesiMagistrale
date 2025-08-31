e9292471d713ce4d1ba2c3b287aadb36fe8a26e8
Zhria <traka.zakaria@gmail.com>
2025-08-31 18:48:46 +0200

    push automatico

--- e2sim/src/SCTP/e2sim_sctp.cpp
+++ e2sim/src/SCTP/e2sim_sctp.cpp
@@ -47,78 +47,6 @@
 #include <poll.h>
 #include <fcntl.h>
 
-int sctp_start_server(const char *server_ip_str, const int server_port)
-{
-  if(server_port < 1 || server_port > 65535) {
-      LOG_E("Invalid port number (%d). Valid values are between 1 and 65535.\n", server_port);
-      exit(1);
-  }
-
-  int server_fd, af;
-  struct sockaddr* server_addr;
-  size_t addr_len;
-
-  struct sockaddr_in  server4_addr;
-  memset(&server4_addr, 0, sizeof(struct sockaddr_in));
-
-  struct sockaddr_in6 server6_addr;
-  memset(&server6_addr, 0, sizeof(struct sockaddr_in6));
-
-  if(inet_pton(AF_INET, server_ip_str, &server4_addr.sin_addr) == 1)
-  {
-    server4_addr.sin_family = AF_INET;
-    server4_addr.sin_port   = htons(server_port);
-
-    server_addr = (struct sockaddr*)&server4_addr;
-    af          = AF_INET;
-    addr_len    = sizeof(server4_addr);
-  }
-  else if(inet_pton(AF_INET6, server_ip_str, &server6_addr.sin6_addr) == 1)
-  {
-    server6_addr.sin6_family = AF_INET6;
-    server6_addr.sin6_port   = htons(server_port);
-
-    server_addr = (struct sockaddr*)&server6_addr;
-    af          = AF_INET6;
-    addr_len    = sizeof(server6_addr);
-  }
-  else {
-    perror("inet_pton()");
-    exit(1);
-  }
-
-  if((server_fd = socket(af, SOCK_STREAM, IPPROTO_SCTP)) == -1) {
-    perror("socket");
-    exit(1);
-  }
-
-  //set send_buffer
-  // int sendbuff = 10000;
-  // socklen_t optlen = sizeof(sendbuff);
-  // if(getsockopt(server_fd, SOL_SOCKET, SO_SNDBUF, &sendbuff, &optlen) == -1) {
-  //   perror("getsockopt send");
-  //   exit(1);
-  // }
-  // else
-  //   LOG_D("[SCTP] send buffer size = %d\n", sendbuff);
-
-
-  if(bind(server_fd, server_addr, addr_len) == -1) {
-    perror("bind");
-    exit(1);
-  }
-
-  if(listen(server_fd, SERVER_LISTEN_QUEUE_SIZE) != 0) {
-    perror("listen");
-    exit(1);
-  }
-
-  assert(server_fd != 0);
-
-  LOG_I("[SCTP] Server started on %s:%d", server_ip_str, server_port);
-
-  return server_fd;
-}
 
 // Stampa gli eventi SCTP dal socket fd (non blocca se non ci sono eventi)
 void sctp_print_events(int fd)
@@ -248,9 +176,9 @@ int sctp_start_client(const char *server_ip_str, const int server_port)
 
     // Attendi il completamento della connect
     struct pollfd p{ .fd = fd, .events = POLLOUT, .revents = 0 };
-    rc = poll(&p, 1, 50000); // timeout 50s
+    rc = poll(&p, 1, 5000); // timeout 5s
     if (rc == 0) {
-        fprintf(stderr, "FAILED (timeout 50000 ms)\n");
+        fprintf(stderr, "FAILED (timeout 5000 ms)\n");
         close(fd);
         return -1;
     }
@@ -277,40 +205,6 @@ int sctp_start_client(const char *server_ip_str, const int server_port)
     (void)set_nonblock(fd, false);
 
     return fd;
-}
-
-int sctp_accept_connection(const char *server_ip_str, const int server_fd)
-{
-  LOG_I("[SCTP] Waiting for new connection...");
-
-  struct sockaddr client_addr;
-  socklen_t       client_addr_size;
-  int             client_fd;
-
-  //Blocking call
-  client_fd = accept(server_fd, &client_addr, &client_addr_size);
-  fprintf(stderr, "client fd is %d\n", client_fd);
-  if(client_fd == -1){
-    perror("accept()");
-    close(client_fd);
-    exit(1);
-  }
-
-  //Retrieve client IP_ADDR
-  char client_ip6_addr[INET6_ADDRSTRLEN], client_ip4_addr[INET_ADDRSTRLEN];
-  if(strchr(server_ip_str, ':') != NULL) //IPv6
-  {
-    struct sockaddr_in6* client_ipv6 = (struct sockaddr_in6*)&client_addr;
-    inet_ntop(AF_INET6, &(client_ipv6->sin6_addr), client_ip6_addr, INET6_ADDRSTRLEN);
-    LOG_I("[SCTP] New client connected from %s", client_ip6_addr);
-  }
-  else {
-    struct sockaddr_in* client_ipv4 = (struct sockaddr_in*)&client_addr;
-    inet_ntop(AF_INET, &(client_ipv4->sin_addr), client_ip4_addr, INET_ADDRSTRLEN);
-    LOG_I("[SCTP] New client connected from %s", client_ip4_addr);
-  }
-
-  return client_fd;
 }
 
 int sctp_send_data(int &socket_fd, sctp_buffer_t &data)
