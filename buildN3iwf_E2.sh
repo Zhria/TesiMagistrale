2b2297c92e7b59396036ca1987cca9eb31ae3271
Zhria <traka.zakaria@gmail.com>
2025-09-05 13:42:35 +0200

    push automatico

--- buildN3iwf_E2.sh
+++ buildN3iwf_E2.sh
@@ -3,6 +3,7 @@ sudo docker build -t n3iwf_local -f ./n3iwfCustom/Dockerfile ./n3iwfCustom/;
 sudo docker build -t n3iwf_local2 -f ./nf_n3iwf/Dockerfile ./nf_n3iwf/;
 sudo docker compose -f dcb.yaml build;
 
+sudo chmod +x ./es2im/build_es2im;
 
 #Build di E2 Node
 #cd ./e2sim
