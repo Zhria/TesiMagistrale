6dec7c870fd1946867dd38c22784299ccb98c09c
Zhria <traka.zakaria@gmail.com>
2025-09-06 16:51:44 +0200

    push automatico

--- e2sim/src/SCTP/e2sim_sctp.cpp
+++ e2sim/src/SCTP/e2sim_sctp.cpp
@@ -194,8 +194,8 @@ int sctp_start_client(const char *server_ip_str, const int server_port)
 
     // Parametri iniziali (facoltativi)
     struct sctp_initmsg init = {0};
-    init.sinit_num_ostreams = 1;
-    init.sinit_max_instreams = 1;
+    init.sinit_num_ostreams = 2;
+    init.sinit_max_instreams = 2;
     init.sinit_max_attempts = 8;
     init.sinit_max_init_timeo = 8000;
     setsockopt(fd, IPPROTO_SCTP, SCTP_INITMSG, &init, sizeof(init));
