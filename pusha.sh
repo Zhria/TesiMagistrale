49fbcd8bc29de37c5914f353de3e2d098e637922
Zhria <traka.zakaria@gmail.com>
2025-11-04 14:22:47 +0100

    Rimozione build fisso --no-build

--- pusha.sh
+++ pusha.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-# Se passo un argomento uso quello, altrimenti metto un messaggio di default
-if [ -n "$1" ]; then
-    COMMIT_MSG="$1"
-else
-    COMMIT_MSG="push automatico"
+COMMIT_MSG="push automatico"
+BUILD_IMAGES=true
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --no-build|-n)
+      BUILD_IMAGES=false
+      shift
+      ;;
+    *)
+      COMMIT_MSG="$*"
+      break
+      ;;
+  esac
+done
+
+if $BUILD_IMAGES; then
+  docker build -t zhria/n3iwfcustom:latest -f ./n3iwfCustom/Dockerfile .
+  docker build -t zhria/amfcustom:latest -f ./amfCustom/Dockerfile .
+  docker build -t zhria/smfcustom:latest -f ./smfCustom/Dockerfile .
+  docker push zhria/n3iwfcustom:latest
+  docker push zhria/amfcustom:latest
+  docker push zhria/smfcustom:latest
 fi
-
-# Aggiungo la parte di build e upload delle immagini custom create per il progetto.
-# Le parti sono n3iwfCustom, amfCustom e smfCustom.
-
-docker build -t zhria/n3iwfcustom:latest -f ./n3iwfCustom/Dockerfile .
-docker build -t zhria/amfcustom:latest -f ./amfCustom/Dockerfile .
-docker build -t zhria/smfcustom:latest -f ./smfCustom/Dockerfile .
-
-docker push zhria/n3iwfcustom:latest
-docker push zhria/amfcustom:latest
-docker push zhria/smfcustom:latest
 
 git add .
 git commit -m "$COMMIT_MSG"
