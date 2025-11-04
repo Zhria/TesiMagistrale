f9f34a51820b7f323d3e771a70734f7c70ac2530
Zhria <traka.zakaria@gmail.com>
2025-11-04 14:26:47 +0100

    Rimozione build fisso

--- buildN3iwf_E2.sh
+++ buildN3iwf_E2.sh
@@ -1,15 +1,13 @@
 #Build di n3iwf
-sudo docker build -t n3iwf_local -f ./n3iwfCustom/Dockerfile ./n3iwfCustom/;
+#sudo docker build -t n3iwf_local -f ./n3iwfCustom/Dockerfile ./n3iwfCustom/;
 sudo docker build -t n3iwf_local2 -f ./nf_n3iwf/Dockerfile ./nf_n3iwf/;
-sudo docker compose -f dcb.yaml build;
 
 #Build di AMF
-sudo docker build -t amf_local -f ./amfCustom/Dockerfile ./amfCustom/;
+#sudo docker build -t amf_local -f ./amfCustom/Dockerfile ./amfCustom/;
 sudo docker build -t amf_local2 -f ./nf_amf/Dockerfile ./nf_amf/;
-sudo docker compose -f dcb.yaml build;
 
 #Build di SMF
-sudo docker build -t smf_local -f ./smfCustom/Dockerfile ./smfCustom/;
+#sudo docker build -t smf_local -f ./smfCustom/Dockerfile ./smfCustom/;
 sudo docker build -t smf_local2 -f ./nf_smf/Dockerfile ./nf_smf/;
 sudo docker compose -f dcb.yaml build;
 
