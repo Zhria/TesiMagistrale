ec617504eb719b3146e6a368b87b44a504b9d70d
Zhria <traka.zakaria@gmail.com>
2025-11-16 19:14:19 +0100

    push automatico

--- e2sim/kpm_callbacks.cpp
+++ e2sim/kpm_callbacks.cpp
@@ -472,14 +472,13 @@ static bool extract_meas_names_from_kpm_actiondef(const OCTET_STRING_t *act_def,
   E2SM_KPM_ActionDefinition_Format1_t *f1 = nullptr;
 
   if (ad->actionDefinition_formats.present ==
-      E2SM_KPM_ActionDefinition__actionDefinition_formats_PR_actionDefinition_Format1)
+      E2SM_KPM_ActionDefinition__actionDefinition_formats_PR_actionDefinition_Format2)
   {
     f1 = ad->actionDefinition_formats.choice.actionDefinition_Format1;
   }
-  else/* if (ad->actionDefinition_formats.present ==
+  else if (ad->actionDefinition_formats.present ==
            E2SM_KPM_ActionDefinition__actionDefinition_formats_PR_actionDefinition_Format4)
-  */
- {
+  {
     E2SM_KPM_ActionDefinition_Format4_t *f4 =
         ad->actionDefinition_formats.choice.actionDefinition_Format4;
     if (f4)
